367c3a7ba16c369d89f1542af4191f597c89dda0
Hermann <blumh@ethz.ch>
2024-05-08 17:53:54 +0200

    close mobile menu on click

--- static/js/hamburger.js
+++ static/js/hamburger.js
@@ -1,31 +1,35 @@
 (function () {
     var menuTrigger = document.querySelector("#toggle-menu-main-mobile");
-  
+
     if (menuTrigger !== null) {
       var body = document.querySelector("body");
       var menuContainer = document.querySelector("#menu-main-mobile");
       var menuContainerCenter = document.querySelector(".menu-main-mobile-center");
+      var menuItems = menuContainerCenter.querySelectorAll("a");
       var closeIcon = document.querySelector("#close-overlay");
       var hamburgerIcons = document.querySelectorAll(".hamburger");
-  
+
       function toggleMobileMenu(e) {
         menuContainer.classList.toggle("open");
         hamburgerIcons.forEach((icon) => icon.classList.toggle("is-active"));
         menuTrigger.classList.toggle("open");
         body.classList.toggle("lock-scroll");
       }
-  
+
       menuTrigger.addEventListener("click", (e) => toggleMobileMenu(e));
-  
+
       function closeOverlay(e) {
         if (e.target === e.currentTarget) {
           toggleMobileMenu();
         }
       }
-  
+
       menuContainerCenter.addEventListener("click", (e) => closeOverlay(e));
       closeIcon.addEventListener("click", (e) => toggleMobileMenu(e));
-  
+      menuItems.forEach((item) => {
+        item.addEventListener("click", (e) => toggleMobileMenu(e));
+      });
+
       document.addEventListener("keydown", (e) => {
         if (e.key === "Escape" || e.key == "Esc") {
           //if esc key was not pressed in combination with ctrl or alt or shift
@@ -38,4 +42,3 @@
       });
     }
   })();
-  
